f1dd3863c04d905abcc9bf8c28ae9998d06ea1fc
Artsiom <c3k00@proton.me>
2023-08-03 20:53:53 +0300

    task

--- Task32.py
+++ Task32.py
@@ -5,3 +5,15 @@
 # [4,6]
 # Вывод:
 # 1, 3
+
+N = int(input("Введите количество элементов: "))
+
+arr = []
+for i in range(N):
+    arr.append(int(input("arr[{}] = ".format(i))))
+
+print(arr)
+
+mini = int(input('Min: '))
+maxi = int(input('Max: '))
+print(*[i for i in range(len(arr)) if mini <= arr[i] <= maxi],sep=', ')
